10aabc0659aba576c4c1d4c6ff02c353ed8a9ea9
sairohith <22pa1a1285@vishnu.edu.in>
2024-06-23 16:25:12 +0530

    d changing

--- models/Vendor.js
+++ models/Vendor.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const firmController = require('../controllers/firmController');
-const verifyToken = require('../middlewares/verifyToken');
+const verifyToken = require('../middleware/verifyToken');
 
 
 const router = express.Router()
